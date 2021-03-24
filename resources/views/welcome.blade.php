ec3b8f567e22d84b612594d07c36cc1c04db2d87
Danish <danishrmemon@gmail.com>
2021-03-24 15:18:26 +0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-12">
+            <h1  class="max-w-6xl mx-auto sm:px-6 lg:px-8">Cashback Reborn</h1>
+        </div>
+    </div>
+</div>
+
+@endsection
