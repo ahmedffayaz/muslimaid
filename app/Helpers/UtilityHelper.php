6d84730f73c148a13516b498c6f88b35b9a83e96
umairtrs <muhammad.umair@therightsw.com>
2023-01-11 12:33:22 +0500

    remove commented code

--- app/Helpers/UtilityHelper.php
+++ app/Helpers/UtilityHelper.php
@@ -1,34 +1,16 @@
 <?php
 
 use Carbon\Carbon;
-use App\Models\Blog;
-use App\Models\Page;
-// use Intervention\Image\Image;
 use App\Models\Store;
 use App\Models\Category;
 use App\Models\UserVerify;
 use Illuminate\Support\Str;
 use App\Models\EmailTemplate;
-use Harimayco\Menu\Models\Menus;
-use Harimayco\Menu\Models\MenuItems;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManagerStatic as Image;
-
-// function ccEmails() {
-//     return config('mail.toSend.ccs');
-// }
-
-// function bccEmails() {
-//     return config('mail.toSend.bcc');
-// }
-
-// function allEmails() {
-//     return config('mail.toSend.all');
-// }
 
 /**
  * get User Full name
@@ -59,10 +41,10 @@ function getUserRole($user)
  * @param $date
  * @return $date
  */
-function convertDate($date, $format=true)
+function convertDate($date, $format = true)
 {
     if ($date !== null) {
-        if($format)
+        if ($format)
             return Carbon::parse($date)->format('d M, Y');
 
         return Carbon::parse($date);
@@ -145,7 +127,7 @@ function saveDocument($file, $directory)
     }
     $filename = Str::random() . time() . '.' . $file->getClientOriginalExtension();
     Storage::disk('public')->putFileAs($directory, $file, $filename);
-    return $path = $directory.'/'.$filename;
+    return $path = $directory . '/' . $filename;
 }
 
 /**
@@ -286,70 +268,72 @@ function getEventsForMenu()
 
 function getCategories()
 {
-    $categories = Category::where('parent_id',0)->orderBy('name', 'ASC')->get();
+    $categories = Category::where('parent_id', 0)->orderBy('name', 'ASC')->get();
     return $categories;
 }
 
-function SiteSetting(){
-    return \App\Models\SiteSetting::latest()->get()->pluck('value','type');
+function SiteSetting()
+{
+    return \App\Models\SiteSetting::latest()->get()->pluck('value', 'type');
 }
-function currency(){
+function currency()
+{
     $settings = SiteSetting();
-$currency = \App\Models\Currency::where('id',$settings['currency'])->pluck('symbol')->first();
-return $currency;
-
+    $currency = \App\Models\Currency::where('id', $settings['currency'])->pluck('symbol')->first();
+    return $currency;
 }
 
-// config()->set('settings',$settings);
-// $currency = \App\Models\Currency::where('id',$settings['currency'])->pluck('symbol')->first();
-// config()->set('currency',$currency);
-
-function sidebarCategories(){
-    $sidebar_categories = Category::where('feature_sidebar',1)->orderBy('name', 'ASC')->get();
+function sidebarCategories()
+{
+    $sidebar_categories = Category::where('feature_sidebar', 1)->orderBy('name', 'ASC')->get();
     return $sidebar_categories;
 }
 
-function sidebarStores(){
-    $stores = Store::where('feature_sidebar',1)->latest()->get();
+function sidebarStores()
+{
+    $stores = Store::where('feature_sidebar', 1)->latest()->get();
     return $stores;
 }
-function textHighlight($text,$search,$highlightColor='#3366cc',$casesensitive=false)
+function textHighlight($text, $search, $highlightColor = '#3366cc', $casesensitive = false)
 {
     return preg_replace('/(' . $search . ')/i', "<span class='color-primary'>$1</span>", $text);
 }
 
-function similarStores($store){
+function similarStores($store)
+{
     $categoryIds = $store->categories->pluck('id')->toArray();
 
     $similarStores = Store::whereHas('categories', function ($query) use ($categoryIds) {
         return $query->whereIn('categories.id', $categoryIds);
-    })->where('id','!=', $store->id)
+    })->where('id', '!=', $store->id)
         ->limit(10)
         ->get();
     return $similarStores;
 }
 
 
-function maintenance(){
+function maintenance()
+{
     if (file_exists(storage_path('framework/down'))) {
         return true;
-    }
-    else
-     return false;
+    } else
+        return false;
 }
 
-function isFacebookEnabled(){
-    if(isset(SiteSetting()['facebook_client_id']) && isset(SiteSetting()['facebook_client_secret']) && isset(SiteSetting()['facebook_url'])){
+function isFacebookEnabled()
+{
+    if (isset(SiteSetting()['facebook_client_id']) && isset(SiteSetting()['facebook_client_secret']) && isset(SiteSetting()['facebook_url'])) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-function isGoogleEnabled(){
-    if(isset(SiteSetting()['google_client_id']) && isset(SiteSetting()['google_client_secret']) && isset(SiteSetting()['google_url'])){
+function isGoogleEnabled()
+{
+    if (isset(SiteSetting()['google_client_id']) && isset(SiteSetting()['google_client_secret']) && isset(SiteSetting()['google_url'])) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -358,19 +342,17 @@ function checkStaticpageRule($url)
 {
     $slug = request()->route('slug');
     $current_route_name = Request::route()->getName();
-    $seo_rules = App\Models\Seo_rule::where('is_enabled', 1)->with('ruleData')->where('url',$url)->first();
-    if($seo_rules != null)
-    {
+    $seo_rules = App\Models\Seo_rule::where('is_enabled', 1)->with('ruleData')->where('url', $url)->first();
+    if ($seo_rules != null) {
         $meta_description = [];
         $meta_keyword = [];
-        $title = Str::title(str_replace('-' , ' ', $slug));
-        foreach($seo_rules->ruleData as $rule)
-        {
-            $rule['key'] == 'meta_description'?  $meta_description[] =$rule['value'] : '';
-            $rule['key'] == 'meta_keyword'?  $meta_keyword[] =$rule['value'] : '';
+        $title = Str::title(str_replace('-', ' ', $slug));
+        foreach ($seo_rules->ruleData as $rule) {
+            $rule['key'] == 'meta_description' ?  $meta_description[] = $rule['value'] : '';
+            $rule['key'] == 'meta_keyword' ?  $meta_keyword[] = $rule['value'] : '';
         }
-        return  ['title' => $title, 'meta_description' =>implode( ',' , $meta_description ) , 'meta_keyword'=>implode( ',' , $meta_keyword ) ];
-    }elseif($slug){
+        return  ['title' => $title, 'meta_description' => implode(',', $meta_description), 'meta_keyword' => implode(',', $meta_keyword)];
+    } elseif ($slug) {
         $route_names = [
             'post' => '\App\Models\Blog',
             'page' => '\App\Models\Page',
@@ -384,56 +366,48 @@ function checkStaticpageRule($url)
                 $meta_keyword = [];
                 $store = Store::where('slug', $slug)->select('id', 'name')->with('storeRuleData')->first();
                 $title = $store->name;
-                foreach($store->storeRuleData as $meta_data) {
+                foreach ($store->storeRuleData as $meta_data) {
                     $meta_data['key'] == 'meta:description' ? $meta_description[] = $meta_data['value'] : '';
                     $meta_data['key'] == 'meta:keywords' ? $meta_keyword[] = $meta_data['value'] : '';
                 }
                 return ['title' => $title, 'meta_description' => implode(',', $meta_description), 'meta_keyword' => implode(',', $meta_keyword)];
-            }elseif ($route_name == $current_route_name){
-                $record = $model::where('slug',$slug)->first();
+            } elseif ($route_name == $current_route_name) {
+                $record = $model::where('slug', $slug)->first();
                 if (($record->title ? $record->title : $record->name) || $record->meta_description && $record->meta_keyword) {
                     return $record;
                 }
                 return null;
             }
         }
-    }else{
+    } else {
         return null;
     }
 }
 
-   function sendVerificationEmail($user)
-   {
-    $verification_email_temp = EmailTemplate::where('key','email_verification')->first();
+function sendVerificationEmail($user)
+{
+    $verification_email_temp = EmailTemplate::where('key', 'email_verification')->first();
 
     $token = Str::random(64);
 
     UserVerify::create([
-          'user_id' => $user->id,
-          'token' => $token
-        ]);
+        'user_id' => $user->id,
+        'token' => $token
+    ]);
 
-    $link = url('').'/account/verify/'.$token;
-    $button = '<a href="'.$link.'" target="_blank"><input type="button" class="btn btn-success" value="Verify"></a>';
-    $filtered_message  = str_replace(['{{SITE_TITLE}}', '{{SITE_URL}}', '{{BUTTON}}'],[SiteSetting()['website_title'], url('/'), $button],$verification_email_temp->message );
+    $link = url('') . '/account/verify/' . $token;
+    $button = '<a href="' . $link . '" target="_blank"><input type="button" class="btn btn-success" value="Verify"></a>';
+    $filtered_message  = str_replace(['{{SITE_TITLE}}', '{{SITE_URL}}', '{{BUTTON}}'], [SiteSetting()['website_title'], url('/'), $button], $verification_email_temp->message);
     $data = array(
-        'email'=> $user->email,
-        'email_message'=>$filtered_message,
-        'subject'=>$verification_email_temp->subject
+        'email' => $user->email,
+        'email_message' => $filtered_message,
+        'subject' => $verification_email_temp->subject
     );
     Mail::send('emails.email_template', $data, function ($message) use ($data) {
         $message->to($data['email'])
             ->subject($data['subject']);
     });
-   }
-
-// function isAppleEnabled(){
-//     if(SiteSetting()['apple_client_id'] && SiteSetting()['apple_client_secret'] && SiteSetting()['apple_url']){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
+}
 
 function safeParseUrl($url)
 {
