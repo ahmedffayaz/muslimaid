cbc2474ac28608c859652d44ef34a69e176adbe1
Iqra Hameed <iqra.hameed@therightsw.com>
2023-01-12 17:00:18 +0500

    contact_form_id

--- app/Http/Controllers/Frontend/PagesController.php
+++ app/Http/Controllers/Frontend/PagesController.php
@@ -263,7 +263,7 @@ class PagesController extends Controller
         });
         $ticket = new Ticket([
             'title'     => $request->input('subject'),
-            'user_id'   =>  isset(Auth::user()->id) ?(Auth::user()->id):0, 
+            'user_id'   =>  Auth::user()->id, 
            'ticket_id' => strtoupper(Str::random(12)),
             'category_id'  => '4',
             'priority'  => 'high',
