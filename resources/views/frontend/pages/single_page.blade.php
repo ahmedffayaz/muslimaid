97f09e9553203fd19467696f87d50962c01a8a49
Iqra Hameed <iqra.hameed@therightsw.com>
2023-01-16 18:15:41 +0500

    banner image add

--- resources/views/frontend/pages/single_page.blade.php
+++ resources/views/frontend/pages/single_page.blade.php
@@ -2,11 +2,12 @@
 @section('content')
 
 @if(!isset($page->title)){{abort(404)}} @endif
+@if($page->status == 0) {{abort(404)}} @endif
 <div class="block block-product-columns">
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>{{$page->title}}</h1>
+                <h1>{{$page->title}}</h1> 
             </div>
             <div class="col-lg-12">
                 <div id="your_container"> <!-- The element you want to render the content in -->
