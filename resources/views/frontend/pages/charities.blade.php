258213d5ffb75b1bbe11ba1ce87ad61c26ac231e
Iqra Hameed <iqra.hameed@therightsw.com>
2023-01-16 17:10:13 +0500

    banner image add

--- resources/views/frontend/pages/charities.blade.php
+++ resources/views/frontend/pages/charities.blade.php
@@ -26,24 +26,27 @@
                 </ol>
             </nav>
         </div>
-        <div class="container p-2 my-2">
-            <div class="row">
-                <div class="col-12">
-                    <div class="block-finder__body">
-                            <img class="banner__size" style="width:1110px;" src="{{url('storage/photos/static_image_banner.jpg')}}" alt="Charities Image Missing">
-                            <div class="block-finder__header">
-                                <div class="block-finder__title">{{$page->title}}</div>
-                                <div class="block-finder__subtitle"></div>
-                            </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div class="category-text panel mb-4 pt-3">
-                <p>{{$page->description}}</p>
-            </div>
     </div>
 </div>
+{{-- <div class="container p-2 my-2">
+    <div class="row">
+        <div class="col-12">
+            <div class="block-finder__body">
+                    <img class="banner__size" style="width:1110px;" src="{{url('storage/photos/static_image_banner.jpg')}}" alt="Charities Image Missing">
+                    <div class="block-finder__header">
+                        <div class="block-finder__title">{{$page->title}}</div>
+                        <div class="block-finder__subtitle"></div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <div class="category-text panel mb-4 pt-3">
+        <p>{{$page->description}}</p>
+    </div> --}}
+    <div class="container p-2 my-2">
+        @include('layouts.frontend.includes.banners.pages_banner')
+    </div>
 @if(!isset($page->title)){{abort(404)}} @endif
 <div class="block block-product-columns">
     <div class="container">
