f1f3b2a52536c49ab72a430504beb956c049b35d
Iqra Hameed <iqra.hameed@therightsw.com>
2023-01-16 12:39:26 +0500

    removed_statement_from_client

--- routes/web.php
+++ routes/web.php
@@ -270,7 +270,6 @@ Route::namespace('App\Http\Controllers\Client')
         Route::get('change_password', [App\Http\Controllers\Client\DashboardController::class, 'changePassword'])->name('change_password');
         Route::post('users/passwordsave/', [App\Http\Controllers\Client\DashboardController::class, 'savePassword'])->name('save_password');
         Route::resource('withdraw', PaymentController::class);
-        Route::get('statement', [App\Http\Controllers\Client\PaymentController::class, 'statement'])->name('statement');
         Route::get('payment-details', [App\Http\Controllers\Client\PaymentController::class, 'paymentDetails'])->name('payment_details');
         Route::post('payment-save', [App\Http\Controllers\Client\PaymentController::class, 'paymentSave'])->name('payment_save');
         Route::post('cashout', [App\Http\Controllers\Client\PaymentController::class, 'cashout'])->name('cashout');
