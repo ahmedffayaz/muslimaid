bb64b6182708b69d998e5c338518d3a079cbb201
Taimoor Ali <taimoor.ali@therightsw.com>
2023-01-22 10:13:05 +0500

    resources/views/frontend added to git ignore

--- resources/views/frontend/stores/sidebar_widget_subcategories.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="widget-categories__subs" data-collapse-content>
-    <ul>
-        @foreach ($childs as $cat)
-            <li>
-                <a href="{{ route('cashabck', $cat->slug) }}">{{ $cat->name }}</a>
-            </li>
-        @endforeach
-    </ul>
-</div>
