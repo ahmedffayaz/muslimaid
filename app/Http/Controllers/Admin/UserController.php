18b99e520525725b87bd4e231928e81c2f931722
Taimoor Ali <taimoor.ali@therightsw.com>
2023-01-25 12:47:35 +0500

    Admin/UserController reformatted

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -20,42 +20,27 @@ class UserController extends Controller
 {
     function __construct()
     {
-         $this->middleware('permission:view users', ['only' => ['index']]);
-         $this->middleware('permission:edit users', ['only' => ['edit','show','update']]);
-         $this->middleware('permission:add users', ['only' => ['create','Store']]);
-         $this->middleware('permission:delete users', ['only' => ['destroy']]);
-         $this->middleware('permission:change password', ['only' => ['changePassword','savePassword']]);
-         $this->middleware('permission:edit payment info', ['only' => ['paymentInfo','paymentSave']]);
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $route='index';
-        $users = User::role('user')->orderBy('id', 'DESC')->paginate(30);
-        return view('admin-dashboard.users.index', compact('users','route'));
+        $this->middleware('permission:view users', ['only' => ['index']]);
+        $this->middleware('permission:edit users', ['only' => ['edit', 'show', 'update']]);
+        $this->middleware('permission:add users', ['only' => ['create', 'Store']]);
+        $this->middleware('permission:delete users', ['only' => ['destroy']]);
+        $this->middleware('permission:change password', ['only' => ['changePassword', 'savePassword']]);
+        $this->middleware('permission:edit payment info', ['only' => ['paymentInfo', 'paymentSave']]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function index()
+    {
+        $route = 'index';
+        $users = User::role('user')->orderBy('id', 'DESC')->paginate(30);
+
+        return view('admin-dashboard.users.index', compact('users', 'route'));
+    }
+
     public function create()
     {
         return view('admin-dashboard.users.create');
-
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $user =  User::create([
@@ -63,50 +48,31 @@ class UserController extends Controller
             'last_name' => $request->lastname,
             'email' => $request->email,
             'password' => Hash::make('123456789'),
-            'registration_type'=>'sign up',
+            'registration_type' => 'sign up',
             'phone' => $request->phone,
             'address' => $request->address,
             'intro' => $request->intro
         ]);
 
         $user->assignRole('user');
+
         flash()->success('New user added successfully');
         return redirect()->route('admin.users.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(User $user)
     {
         $users = User::role('user')->latest()->get();
         $roles = Role::all();
-        return view('admin-dashboard.users.show', compact('user','users','roles'));
 
+        return view('admin-dashboard.users.show', compact('user', 'users', 'roles'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(User $user)
     {
         return view('admin-dashboard.users.edit', compact('user'));
-
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, User $user)
     {
         $user->update([
@@ -121,57 +87,55 @@ class UserController extends Controller
 
         $user->assignRole($request->roles);
 
-        if(!$request->ajax()){
+        if (!$request->ajax()) {
             flash()->success('User updated successfully');
             return redirect()->route('admin.users.index');
-        }else{
-            return true;
         }
+
+        return true;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(User $user)
     {
-        if($user->paymentInfo){
+        if ($user->paymentInfo) {
             $user->paymentInfo->delete();
-
         }
+
         $user->delete();
 
         flash()->success('User deleted successfully');
         return redirect()->route('admin.users.index');
     }
+
     function fetch(Request $request)
     {
-        if($request->ajax())
-        {
-            $route="index";
-            $users = User::role('user')->orderBy('id', 'DESC')->paginate(30);
+        if (!$request->ajax()) return;
 
-            return view('admin-dashboard.users.index_data', compact('users','route'))->render();
-        }
+        $route = "index";
+        $users = User::role('user')->orderBy('id', 'DESC')->paginate(30);
+
+        return view('admin-dashboard.users.index_data', compact('users', 'route'))->render();
     }
+
     public function exportCsv(Request $request)
     {
         try {
-            
             $table = User::role('user')->latest()->get();
             $filename = "users.csv";
-            $handle = fopen($filename, 'w+');
-            fputcsv($handle, array('First Name', 'Last Name','Cashback Amount', 'Reg Type','Reg date', 'Status'));
 
-            foreach($table as $row) {
-                fputcsv($handle, array($row->first_name,
-                                        $row->last_name,
-                                        '0',
-                                        $row->registration_type, 
-                                        $row->created_at,
-                                        $row->status ? 'active' : 'in-active'));
+            $handle = fopen($filename, 'w+');
+
+            fputcsv($handle, array('First Name', 'Last Name', 'Cashback Amount', 'Reg Type', 'Reg date', 'Status'));
+
+            foreach ($table as $row) {
+                fputcsv($handle, array(
+                    $row->first_name,
+                    $row->last_name,
+                    '0',
+                    $row->registration_type,
+                    $row->created_at,
+                    $row->status ? 'active' : 'in-active'
+                ));
             }
 
             fclose($handle);
@@ -181,46 +145,39 @@ class UserController extends Controller
             );
 
             return Response::download($filename, 'users.csv', $headers);
-
         } catch (\Throwable $th) {
-
             flash()->error('Error while exporting the users');
             return redirect()->route('admin.users.index');
-
         }
-        
     }
 
     public function paymentInfo(User $user)
     {
-        return view('admin-dashboard.users.payment_info',compact('user'));
+        return view('admin-dashboard.users.payment_info', compact('user'));
     }
 
-
-    public function paymentSave(Request $request){
-
-        $payment = PaymentInfo::updateOrCreate([
+    public function paymentSave(Request $request)
+    {
+        PaymentInfo::updateOrCreate([
             'user_id'   => $request->user_id,
-        ],$request->all());
+        ], $request->all());
 
-        // $payment = PaymentInfo::create($request->all());
-
-        if(!$request->ajax())
-        {
+        if (!$request->ajax()) {
             flash()->success('Payment method added successfully');
             return redirect()->route('admin.users.index');
-        }else{
-            return array('message'=>'payment method saved',
-                        'updated'=>'success');
         }
-        
-        
 
+        return array(
+            'message' => 'payment method saved',
+            'updated' => 'success'
+        );
     }
+
     public function changePassword(User $user)
     {
-        return view('admin-dashboard.users.password',compact('user'));
+        return view('admin-dashboard.users.password', compact('user'));
     }
+
     public function savePassword(Request $request, User $user)
     {
         $validator = Validator::make($request->all(), [
@@ -228,83 +185,75 @@ class UserController extends Controller
         ]);
 
         if ($validator->fails()) {
-           
-
-            if(!$request->ajax())
-            {
+            if (!$request->ajax()) {
                 flash()->error($validator->errors()->first());
                 return redirect()->back();
-            }else{
-                return array('message' => $validator->errors()->first(),
-                                'updated'=>'error');
             }
 
+            return array(
+                'message' => $validator->errors()->first(),
+                'updated' => 'error'
+            );
         }
+
         $user->update([
             'password' => Hash::make($request->password),
-
         ]);
-        if(!$request->ajax())
-        {
-        flash()->success('Password changed successfully');
-        return redirect()->route('admin.users.index');
-        }else{
-            return array('message' => 'Password updated successfully',
-                                'updated'=>'success');
+
+        if (!$request->ajax()) {
+            flash()->success('Password changed successfully');
+            return redirect()->route('admin.users.index');
         }
 
+        return array(
+            'message' => 'Password updated successfully',
+            'updated' => 'success'
+        );
     }
+
     public function searchUsers(Request $request, User $users)
     {
-        // dd($request->all());
         $users = $users->newQuery();
-
-        // Search by type.
-        // if ($request->input('type')!=-1) {
-        //     $users->where('registration_type', $request->input('type'));
-        // }
 
         // Search by store name/ID.
         if ($request->input('name')) {
-            $users->where(DB::raw("CONCAT(first_name,' ',last_name)"), 'like', '%'.$request->input('name').'%');
-            $users->orWhere('id',$request->input('name'));
-           
+            $users->where(DB::raw("CONCAT(first_name,' ',last_name)"), 'like', '%' . $request->input('name') . '%');
+            $users->orWhere('id', $request->input('name'));
         }
 
-        if($request->input('email')){
-            $users->Where('email','like', '%'.$request->input('email').'%');
+        if ($request->input('email')) {
+            $users->Where('email', 'like', '%' . $request->input('email') . '%');
         }
 
         // Search by status.
-        if ($request->input('status')!=-1) {
+        if ($request->input('status') != -1) {
             $users->where('status', $request->input('status'));
         }
-        
+
         $users = $users->role('user')->orderBy('id', 'DESC')->paginate(30);
-        $route='search';
-        return view('admin-dashboard.users.index_data', compact('users','route'))->render();
+        $route = 'search';
+
+        return view('admin-dashboard.users.index_data', compact('users', 'route'))->render();
     }
 
     function fetchCashbacks(Request $request)
     {
-        if($request->ajax())
-        {
-            $cashbacks = UserCashback::where('user_id',$request->user)->latest()->paginate(20);
-            return view('admin-dashboard.users.cashbacks', compact('cashbacks'))->render();
-        }
-    }
-    function fetchClicks(Request $request)
-    {
-        if($request->ajax())
-        {
-            $clicks = ExitClick::where('user_id',$request->user)->latest()->paginate(20);
-            return view('admin-dashboard.users.clicks', compact('clicks'))->render();
-        }
-    }
-    public function showUser()
-    {
-      
-        return view('admin-dashboard.users.show');
+        if (!$request->ajax()) return;
+
+        $cashbacks = UserCashback::where('user_id', $request->user)->latest()->paginate(20);
+        return view('admin-dashboard.users.cashbacks', compact('cashbacks'))->render();
     }
 
+    function fetchClicks(Request $request)
+    {
+        if (!$request->ajax()) return;
+
+        $clicks = ExitClick::where('user_id', $request->user)->latest()->paginate(20);
+        return view('admin-dashboard.users.clicks', compact('clicks'))->render();
+    }
+
+    public function showUser()
+    {
+        return view('admin-dashboard.users.show');
+    }
 }
