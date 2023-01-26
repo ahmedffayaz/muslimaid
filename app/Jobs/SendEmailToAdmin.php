13d09b34de34cc8f226d4b7aac85b7f5add68f37
umairtrs <muhammad.umair@therightsw.com>
2023-01-26 11:34:27 +0500

    take pull and merge develp, remove extra lines

--- app/Jobs/SendEmailToAdmin.php
+++ app/Jobs/SendEmailToAdmin.php
@@ -8,8 +8,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
-use Illuminate\Support\Facades\Log;
 
 class SendEmailToAdmin implements ShouldQueue
 {
--- app/Jobs/SendEmailToUser.php
+++ app/Jobs/SendEmailToUser.php
@@ -3,12 +3,11 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Log;
 
 class SendEmailToUser implements ShouldQueue
 {
