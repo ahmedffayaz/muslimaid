5680085b8a00981797c16df1b294968de550e67d
umairtrs <muhammad.umair@therightsw.com>
2023-02-01 15:51:07 +0500

    formate document

--- app/Http/Controllers/Frontend/TicketsController.php
+++ app/Http/Controllers/Frontend/TicketsController.php
@@ -13,72 +13,71 @@ use App\Models\TicketCategory;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 
-
-
 class TicketsController extends Controller
 {
     public function create()
     {
         $categories = TicketCategory::all();
         $languages = Language::orderBy('id', 'desc')->get();
-        return view('frontend.tickets.create', compact('categories','languages'));
+        return view('frontend.tickets.create', compact('categories', 'languages'));
     }
+
     public function store(Request $request, AppMailer $mailer)
     {
         $this->validate($request, [
-                'title'     => 'required',
-                // 'category'  => 'required',
-                'message'   => 'required'
-            ]);
+            'title'     => 'required',
+            'message'   => 'required'
+        ]);
 
-            $ticket = new Ticket([
-                'title'     => $request->input('title'),
-                'user_id'   => Auth::user()->id,
-                'ticket_id' => strtoupper(Str::random(12)),
-                'category_id'  => $request->input('category'),
-                'priority'  => 'high',
-                'ticket_type'  => 'ticket',
-                'message'   => $request->input('message'),
-                'status'    => "open",
-            ]);
+        $ticket = new Ticket([
+            'title'     => $request->input('title'),
+            'user_id'   => Auth::user()->id,
+            'ticket_id' => strtoupper(Str::random(12)),
+            'category_id'  => $request->input('category'),
+            'priority'  => 'high',
+            'ticket_type'  => 'ticket',
+            'message'   => $request->input('message'),
+            'status'    => "open",
+        ]);
 
-            $ticket->save();
+        $ticket->save();
 
-            $user_email_template = EmailTemplate::where('key','user_new_ticket')->first(); 
-            $admin_email_template = EmailTemplate::where('key','admin_new_ticket')->first(); 
+        $user_email_template = EmailTemplate::where('key', 'user_new_ticket')->first();
+        $admin_email_template = EmailTemplate::where('key', 'admin_new_ticket')->first();
 
-            $filtered_user_message  = str_replace(['{{SITE_TITLE}}', '{{SITE_URL}}', '{{NAME}}', '{{EMAIL}}','{{TICKET_ID}}', '{{CATEGORY}}','{{MESSAGE}}'],
-                                        [SiteSetting()['website_title'], url('/') ,$ticket->user->first_name.' '.$ticket->user->last_name,$ticket->user->email,$ticket->ticket_id,$ticket->category->name ?? '',$ticket->message],
-                                        $user_email_template->message );
-            $filtered_admin_message  = str_replace(['{{SITE_TITLE}}', '[{SITE_URL}}', '{{NAME}}', '{{EMAIL}}','{{TICKET_ID}}', '{{CATEGORY}}','{{MESSAGE}}'],
-                                        [SiteSetting()['website_title'], url('/') ,$ticket->user->first_name.' '.$ticket->user->last_name,$ticket->user->email,$ticket->ticket_id,$ticket->category->name ?? '',$ticket->message],
-                                        $admin_email_template->message );
+        $filtered_user_message  = str_replace(
+            ['{{SITE_TITLE}}', '{{SITE_URL}}', '{{NAME}}', '{{EMAIL}}', '{{TICKET_ID}}', '{{CATEGORY}}', '{{MESSAGE}}'],
+            [SiteSetting()['website_title'], url('/'), $ticket->user->first_name . ' ' . $ticket->user->last_name, $ticket->user->email, $ticket->ticket_id, $ticket->category->name ?? '', $ticket->message],
+            $user_email_template->message
+        );
+        $filtered_admin_message  = str_replace(
+            ['{{SITE_TITLE}}', '[{SITE_URL}}', '{{NAME}}', '{{EMAIL}}', '{{TICKET_ID}}', '{{CATEGORY}}', '{{MESSAGE}}'],
+            [SiteSetting()['website_title'], url('/'), $ticket->user->first_name . ' ' . $ticket->user->last_name, $ticket->user->email, $ticket->ticket_id, $ticket->category->name ?? '', $ticket->message],
+            $admin_email_template->message
+        );
 
+        $email_data = array(
+            'name' =>  $ticket->user->first_name . ' ' . $ticket->user->last_name,
+            'email' => $ticket->user->email,
+            'email_message' => $filtered_user_message,
+            'subject' => $user_email_template->subject
+        );
+        Mail::send('emails.email_template', $email_data, function ($message) use ($email_data) {
+            $message->to($email_data['email'], $email_data['name'])
+                ->subject($email_data['subject']);
+        });
+        $email_data = array(
+            'name' =>  $ticket->user->first_name . ' ' . $ticket->user->last_name,
+            'email' => $ticket->user->email,
+            'email_message' => $filtered_admin_message,
+            'subject' => $admin_email_template->subject
+        );
+        Mail::send('emails.email_template', $email_data, function ($message) use ($email_data) {
+            $message->to('admin@trs.com', $email_data['name'])
+                ->subject($email_data['subject']);
+        });
 
-            $email_data = array(
-                'name' =>  $ticket->user->first_name.' '.$ticket->user->last_name,
-                'email' => $ticket->user->email,
-                'email_message'=>$filtered_user_message,
-                'subject'=>$user_email_template->subject
-            );
-            Mail::send('emails.email_template', $email_data, function ($message) use ($email_data) {
-                $message->to($email_data['email'], $email_data['name'])
-                    ->subject($email_data['subject']);
-            });
-            $email_data = array(
-                'name' =>  $ticket->user->first_name.' '.$ticket->user->last_name,
-                'email' => $ticket->user->email,
-                'email_message'=>$filtered_admin_message,
-                'subject'=>$admin_email_template->subject
-            );
-            Mail::send('emails.email_template', $email_data, function ($message) use ($email_data) {
-                $message->to('admin@trs.com', $email_data['name'])
-                    ->subject($email_data['subject']);
-            });
-
-
-            flash()->success("A ticket with ID: #$ticket->ticket_id has been opened.");
-
-            return redirect()->back();
+        flash()->success("A ticket with ID: #$ticket->ticket_id has been opened.");
+        return redirect()->back();
     }
 }
