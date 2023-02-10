d3fedd274012a7ab6088c1d5ac212e0c32c68f14
Iqra Hameed <iqra.hameed@therightsw.com>
2023-02-10 11:10:19 +0500

    payment_method bug resolved

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -146,15 +146,6 @@ class SettingsController extends Controller
     public function saveSettings(Request $request)
     {
         try {
-            $validated = $request->validate([
-                'referral_bonus' => 'min:0|numeric',
-                'welcome_bonus' => 'min:0|numeric',
-                'min_cashout_amount'=> 'min:0|numeric',
-            ], $messages = [
-                'referral_bonus' => 'Value must be equal to or greater than 0.',
-                'welcome_bonus' => 'Value must be equal to or greater than 0.',
-                'min_cashout_amount' => 'Value must be equal to or greater than 0.',
-            ]);
             $request->offsetUnset('_method');
             $request->offsetUnset('_token');
 
