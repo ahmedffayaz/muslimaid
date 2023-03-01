2eafcec18ec7d336dae700f3519805860bd2b8ae
umairtrs <muhammad.umair@therightsw.com>
2023-03-01 18:00:39 +0500

    document formatted

--- resources/views/admin-dashboard/menus/index.blade.php
+++ resources/views/admin-dashboard/menus/index.blade.php
@@ -1,28 +1,25 @@
 @extends('layouts.admin-dashboard.app')
 @section('content')
-
-<div class="nk-content ">
-    <div class="container-fluid">
-        <div class="nk-content-inner">
-            <div class="nk-content-body">
-                <div class="components-preview mx-auto">
-                  
-                    
-                  
-                    <div class="nk-block nk-block-lg">
-                        <div class="nk-block-head">
-                            <div class="nk-block-head-content">
-                                <div class="nk-block-des">
+    <div class="nk-content ">
+        <div class="container-fluid">
+            <div class="nk-content-inner">
+                <div class="nk-content-body">
+                    <div class="components-preview mx-auto">
+                        <div class="nk-block nk-block-lg">
+                            <div class="nk-block-head">
+                                <div class="nk-block-head-content">
+                                    <div class="nk-block-des">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card">
-                            <div class="card-inner">
-                                <div class="card-head">
-                                    <h5 class="card-title">Menu</h5>
-                                </div>
+                            <div class="card">
+                                <div class="card-inner">
+                                    <div class="card-head">
+                                        <h5 class="card-title">Menu</h5>
+                                    </div>
 
-                            {!! Menu::render() !!}
+                                    {!! Menu::render() !!}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -30,9 +27,6 @@
             </div>
         </div>
     </div>
-</div>
-                
-
 @endsection
 @push('scripts')
     {!! Menu::scripts() !!}
