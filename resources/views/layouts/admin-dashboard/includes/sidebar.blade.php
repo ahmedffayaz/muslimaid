cade8e99ec0539ea905c6e4cb16637e7b24b2f25
Samra-Mushtaq1 <samra.mushtaq@therightsw.com>
2023-03-03 17:17:41 +0500

    sde bar and header setting

--- resources/views/layouts/admin-dashboard/includes/sidebar.blade.php
+++ resources/views/layouts/admin-dashboard/includes/sidebar.blade.php
@@ -78,6 +78,13 @@ d-xl-none
                                 </a>
                             </li><!-- .nk-menu-item -->
                             @endcan
+                            <!-- .nk-menu-item -->
+                            <li class="nk-menu-item">
+                                <a href="{{ route('admin.charities.index') }}" class="nk-menu-link">
+                                    <span class="nk-menu-icon"><em class="icon ni ni-star"></em></span>
+                                    <span class="nk-menu-text">Charities</span>
+                                </a>
+                            </li><!-- .nk-menu-item -->
                         </ul><!-- .nk-menu-sub -->
                     </li><!-- .nk-menu-item -->
                     @endcanany
@@ -213,13 +220,7 @@ d-xl-none
                                     <span class="nk-menu-icon"><em class="icon ni ni-menu"></em></span>
                                     <span class="nk-menu-text">Menu</span>
                                 </a>
-                            </li><!-- .nk-menu-item -->
-                            <li class="nk-menu-item">
-                                <a href="{{ route('admin.charities.index') }}" class="nk-menu-link">
-                                    <span class="nk-menu-icon"><em class="icon ni ni-star"></em></span>
-                                    <span class="nk-menu-text">Charities</span>
-                                </a>
-                            </li><!-- .nk-menu-item -->
+                            </li>
                         </ul><!-- .nk-menu-sub -->
                     </li><!-- .nk-menu-item -->
                     @endcanany
