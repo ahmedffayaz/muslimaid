3a2dbbad273dfec7358ab205271e4ce7fe0278f5
Samra-Mushtaq1 <samra.mushtaq@therightsw.com>
2023-04-05 09:08:26 +0500

    status batches helper

--- app/Helpers/UtilityHelper.php
+++ app/Helpers/UtilityHelper.php
@@ -35,6 +35,26 @@ function getSpecificSetting($type)
     return $setting;
 }
 
+function statusBatches($status)
+{
+    if ($status == 'confirmed'){
+       return '<span class="badge badge-primary">'.$status.'</span>';
+    }elseif($status == 'paid'){
+       return '<span class="badge badge-success">'.$status.'</span>';
+    }elseif($status == 'failed'){
+        return '<span class="badge badge-danger">'.$status.'</span>';
+    }elseif($status == 'pending'){
+        return '<span class="badge badge-info">'.$status.'</span>';
+    }elseif($status == 'donated'){
+        return '<span class="badge badge-secondary">'.$status.'</span>';
+    }elseif($status == 'processing donation'){
+        return '<span class="badge badge-light">'.$status.'</span>';
+    }elseif($status == 'processing'){
+        return '<span class="badge badge-warning">'.$status.'</span>';
+    }
+    return '<span class="badge badge-primary">'.$status.'</span>';
+}
+
 function getHomeSliders()
 {
     $name = "Before Login Home";
