c1dd8e3e51731b0d881ce5d2f611eefc29654d1b
wuas-mingxin <umayraqib@gmail.com>
2023-04-07 15:51:34 +0500

    Show stores and cashblack to your door filtered by letter, latest, highest by amount and percentage, and show categories and child categories stores

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -17,21 +17,16 @@ class CategoryController extends Controller
 {
     use ApiResponser;
 
-
-    public function index($letter = null)
+    public function index(Request $request)
     {
         try {
             $page = Page::where('slug', 'categories')->whereType('system')->pluck('banner_image')->firstOrFail();
 
-            if (!empty($letter)) {
-                $categories = Category::where('name', 'like', $letter . '%')->with(['childs' => function ($query) {
-                    $query->withCount('stores');
-                }])->withCount('stores')->where('parent_id', 0)->orderBy('sort', 'desc')->orderBy('name', 'asc')->paginate(12);
-            } else {
-                $categories = Category::with(['childs' => function ($query) {
-                    $query->withCount('stores');
-                }])->withCount('stores')->where('parent_id', 0)->orderBy('sort', 'desc')->orderBy('name', 'asc')->paginate(12);
-            }
+            $categories = Category::when($request->has('letter'), function ($query) use ($request) {
+                $query->where('name', 'like', $request->input('letter') . '%');
+            })->with(['childs' => function ($query) {
+                $query->withCount('stores');
+            }])->withCount('stores')->where('parent_id', 0)->paginate(12);
 
             if ($categories->count() == 0) {
                 $data = [
@@ -60,23 +55,16 @@ class CategoryController extends Controller
         }
     }
 
-    public function show($slug, $letter = null)
+    public function show(Request $request, $slug)
     {
         try {
-            if (!empty($letter)) {
-                $stores = Store::where('name', 'like', $letter . '%')
-                    ->whereHas('categories', function ($query) use ($slug) {
-                        $query->whereSlug($slug)->where('parent_id', '!=', 0)->whereStatus(1);
-                    })->with(['categories' => function ($query) use ($slug) {
-                        $query->whereSlug($slug)->whereStatus(1);
-                    }])->whereStatus('active')->paginate(12);
-            } else {
-                $stores = Store::whereHas('categories', function ($query) use ($slug) {
-                    $query->whereSlug($slug)->where('parent_id', '!=', 0)->whereStatus(1);
-                })->with(['categories' => function ($query) use ($slug) {
-                    $query->whereSlug($slug)->where('parent_id', '!=', 0)->whereStatus(1);
-                }])->whereStatus('active')->paginate(12);
-            }
+            $stores = Store::when($request->has('letter'), function ($query) use ($request) {
+                $query->where('name', 'like', $request->input('letter') . '%');
+            })->whereHas('categories', function ($query) use ($slug) {
+                $query->whereSlug($slug)->where('parent_id', '!=', 0)->whereStatus(1);
+            })->with(['categories' => function ($query) use ($slug) {
+                $query->whereSlug($slug)->where('parent_id', '!=', 0)->whereStatus(1);
+            }])->whereStatus('active')->paginate(12);
 
             if ($stores->count() == 0) {
                 $data = [
@@ -106,6 +94,55 @@ class CategoryController extends Controller
                 'status' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
                 'message' => 'Something went wrong, try again'
             ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function getCashblackYourDoor(Request $request, $slug)
+    {
+        try {
+
+            $stores = Store::when($request->has('letter'), function ($query) use ($request) {
+                $query->where('name', 'like', $request->input('letter') . '%');
+            })->when($request->orderBy == 'latest', function ($query) use ($request) {
+                $query->latest();
+            })->when($request->orgerBy == 'popularity', function ($query) use ($request) {
+                $query->withCount('clicks')->orderByDesc('clicks_count');
+            })->when($request->orderBy == 'cashback-amount', function ($query) use ($request) {
+                $query->whereHas('cashbacks', function ($query) {
+                    $query->whereType('fixed');
+                })->get()->filter(function ($query) {
+                    $cashback = $query->getCashback();
+                    return (strpos($cashback, '£') !== false);
+                });
+            })->when($request->orderBy == 'cashback-percentage', function ($query) {
+                $query->whereHas('cashbacks', function ($query) {
+                    $query->where('type', 'percentage');
+                })->get()->filter(function ($query) {
+                    $cashback = $query->getCashback();
+                    return (strpos($cashback, '%') !== false);
+                });
+            })->whereHas('categories', function ($query) use ($slug) {
+                $query->whereSlug($slug)->where('parent_id', 0)->whereStatus(1);
+            })->with(['categories' => function ($query) use ($slug) {
+                $query->whereSlug($slug)->where('parent_id', 0)->whereStatus(1);
+            }])->whereStatus('active')->paginate(12);
+
+            $data = [
+                'status' => JsonResponse::HTTP_OK,
+                'message' => 'Success',
+                'data' => [
+                    'main_banner' => ($stores[0]->categories[0]->banner_type != 'link') ? getBannerImageUrl($stores[0]->categories[0]->banner_upload, 'upload', $stores[0]->categories[0]) : $stores[0]->categories[0]->banner_link,
+                    'categories' => StoreResource::collection($stores)
+                ]
+            ];
+
+            return response()->json($data, JsonResponse::HTTP_OK);
+        } catch (Exception $e) {
+            $data = [
+                'status' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                'message' => 'Something went wrong, try again'
+            ];
+            return response()->json($data, JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
