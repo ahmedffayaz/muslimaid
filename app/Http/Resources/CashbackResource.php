5f1eab342dd306e53d8c68f6c30d759b7b0abae4
Samra-Mushtaq1 <samra.mushtaq@therightsw.com>
2023-04-18 09:43:56 +0500

    cashback tracking url added in resources

--- app/Http/Resources/CashbackResource.php
+++ app/Http/Resources/CashbackResource.php
@@ -18,6 +18,10 @@ class CashbackResource extends JsonResource
             'cashback' => $this->getCashback(),
             'icon' => !empty($this->image) ? getImageUrl(asset('storage/' . $this->image)) : null,
             'detail' => $this->detail,
+            'sale_commission' => empty($this->sale_commission) ? '' : $this->sale_commission,
+            'click_url' => empty($this->click_url) ? '' : $this->click_url,
+            'deeplink_url' => empty($this->deeplink_url) ? '' : $this->deeplink_url,
+            'tracking_url' => empty($this->tracking_url) ? '' : $this->tracking_url,
             "date_updated" => date('d-M-Y', strtotime($this->updated_at)),
             "date_created" => date('d-M-Y', strtotime($this->created_at)),
         ];
