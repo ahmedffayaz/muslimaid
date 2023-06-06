a2c7bebdae85b6b389e0ac817b912f4270084c40
wuas-mingxin <umayraqib@gmail.com>
2023-06-06 17:41:20 +0500

    added networks in modules.yml file, resolve CJ network error from importer, change if statement from network modal

--- resources/views/admin-dashboard/networks/importer_setting_form.blade.php
+++ resources/views/admin-dashboard/networks/importer_setting_form.blade.php
@@ -4,22 +4,22 @@
     <input type="hidden" name="network_name" value="{{$network->name}}">
     @if(getImporterYMLSettings('Networks_'. $network->name . '_Importer_Stores' ))
     <div class="custom-control custom-control-sm custom-checkbox mr-2">
-        <input @if (@$network->importerSetting->import_stores) checked @endif type="checkbox" class="custom-control-input" id="stores" name="stores"
-            {{ in_array(strtolower($network->name), ['awin', 'cj', 'webgains']) ? '' : 'disabled' }}>
+        <input @if (@$network->importerSetting->import_stores) checked @endif type="checkbox"
+            class="custom-control-input" id="stores" name="stores">
         <label class="custom-control-label" for="stores">Stores</label>
     </div>
     @endif
-    @if (!(strpos(strtolower($network), 'awin') !== false) && getImporterYMLSettings('Networks_'. $network->name . '_Importer_Vouchers' ))
+    @if (getImporterYMLSettings('Networks_'. $network->name . '_Importer_Vouchers' ))
     <div class="custom-control custom-control-sm custom-checkbox mr-2">
         <input @if(@$network->importerSetting->import_vouchers) checked @endif type="checkbox"
-        class="custom-control-input" id="vouchers" name="vouchers">
+            class="custom-control-input" id="vouchers" name="vouchers">
         <label class="custom-control-label" for="vouchers">Vouchers</label>
     </div>
     @endif
     @if(getImporterYMLSettings('Networks_'. $network->name . '_Importer_Cashbacks' ))
     <div class="custom-control custom-control-sm custom-checkbox mr-2">
-        <input @if (@$network->importerSetting->import_cashbacks) checked @endif type="checkbox" class="custom-control-input" id="cashback" name="cashback"
-            {{ in_array(strtolower($network->name), ['awin', 'cj', 'webgains']) ? '' : 'disabled' }}>
+        <input @if (@$network->importerSetting->import_cashbacks) checked @endif type="checkbox"
+            class="custom-control-input" id="cashback" name="cashback">
         <label class="custom-control-label" for="cashback">Cashbacks</label>
     </div>
     @endif
