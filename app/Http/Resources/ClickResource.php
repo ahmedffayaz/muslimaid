e6b4e02fa3d8a0b1466663ad8b6d3feaeb72e94b
Iqra Hameed <iqra.hameed@therightsw.com>
2023-06-08 11:43:30 +0500

    add slug in click category, parent id remove bcoz it works for both parent and choild category, add lat long in store respurce

--- app/Http/Resources/ClickResource.php
+++ app/Http/Resources/ClickResource.php
@@ -17,6 +17,7 @@ class ClickResource extends JsonResource
         return [
             'id' => $this->store->id,
             'title' => $this->store->name,
+            'url_key' => $this->store->slug,
             'date' => \Carbon\Carbon::parse($this->created_at)->isoFormat('Do MMMM YYYY'),
             'converted' => isset($this->cashback) ? 'Converted' : 'Not Converted'
         ];
