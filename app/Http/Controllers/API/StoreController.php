b88bd5d41ab94f4692be1c91a126e407ff120082
Ma Mingxin <muhammad.umair@therightsw.com>
2023-07-18 16:03:21 +0500

    add message error in api

--- app/Http/Controllers/API/StoreController.php
+++ app/Http/Controllers/API/StoreController.php
@@ -113,7 +113,7 @@ class StoreController extends Controller
 
             $data = [
                 'status' => 500,
-                'message' => 'Something went wrong, try again.',
+                'message' => $e->getMessage() . 'Something went wrong, try again.',
                 'data' => []
             ];
             return response()->json($data, 500);
