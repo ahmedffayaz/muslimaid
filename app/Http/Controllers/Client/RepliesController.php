172575861e57d478d9bf31f305b69ab8893c8a56
Ma Mingxin <muhammad.umair@therightsw.com>
2023-09-05 18:36:21 +0500

    Show error or success message on submit ticket and send email

--- app/Http/Controllers/Client/RepliesController.php
+++ app/Http/Controllers/Client/RepliesController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use App\Jobs\SendNotification;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
@@ -30,20 +32,49 @@ class RepliesController extends Controller
                 ->withInput()
                 ->with('error', 'Please ensure that the line is no longer than 225 characters.');
         }
-        $reply = TicketReply::create([
-            'reply' =>  $request->input('reply'),
-            'user_id' => Auth::user()->id,
-            'ticket_id' => $request->input('ticket_id'),
-            'reply_by' => 'user'
-        ]);
-        $reply->ticket->update(['status' => 'pending']);
-        $title = 'Ticket Replied';
-        $message = 'User replied to the ticket';
-        $url = url('account/tickets');
-        $user = User::first();
-        $deviceToken = optional($user->devices()->whereType('web')->first())->fcm_token;
 
-        $deviceToken != null ? dispatch(new SendNotification($title, $message, $deviceToken, $url, $user)) : '';
+        try {
+            $reply = TicketReply::create([
+                'reply' =>  $request->input('reply'),
+                'user_id' => Auth::user()->id,
+                'ticket_id' => $request->input('ticket_id'),
+                'reply_by' => 'user'
+            ]);
+
+            $ticket = Ticket::findOrFail($request->ticket_id);
+
+            $reply->ticket->update(['status' => 'pending']);
+
+            sendEmailNotification($ticket);
+
+            $title = 'Ticket Replied';
+            $message = 'User replied to the ticket';
+            $url = url('account/tickets');
+            $user = User::first();
+            $deviceToken = optional($user->devices()->whereType('web')->first())->fcm_token;
+
+            $deviceToken != null ? dispatch(new SendNotification($title, $message, $deviceToken, $url, $user)) : '';
+
+            if (!$request->ajax()) {
+                flash()->success('Ticket has been replied successfully');
+                return back();
+            } else {
+                return response()->json([
+                    'status' => JsonResponse::HTTP_OK,
+                    'message' => 'Ticket has been replied successfully'
+                ], JsonResponse::HTTP_OK);
+            }
+        } catch (Exception $e) {
+            if (!$request->ajax()) {
+                flash()->success('Something went wrong');
+                return back();
+            } else {
+                return response()->json([
+                    'status' => JsonResponse::HTTP_INTERNAL_SERVER_ERROR,
+                    'message' => 'Something went wrong'
+                ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
         return back();
     }
 }
