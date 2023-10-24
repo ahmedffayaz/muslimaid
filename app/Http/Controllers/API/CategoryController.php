92120103ba8ac42d3319e82ce98bcaba8d7b0527
Hamid Shaukat Amin <80332339+Akhwandzada1@users.noreply.github.com>
2023-10-24 14:40:40 +0500

    Updating the category API to accomodate the parent categories with more visibility in API response by adding another key of more_categories and then testing it out to make sure it works perfect

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -39,18 +39,16 @@ class CategoryController extends Controller
             ->appends(request()->input());
             
             // Get the 'more' category separately
-            $moreCategory = Category::where('name', 'more')
-                ->with(['childs' => function ($query) {
-                    $query->orderBy('name', 'asc')->withCount('stores');
-                }])
-                ->withCount('stores')
-                ->where('parent_id', 0)
-                ->first();
+            $moreCategories = Category::where('name', 'more')
+            ->orWhere('name', '!=', 'more')
+            ->where('visibility', 'more')
+            ->where('parent_id', 0)
+            ->with(['childs' => function ($query) {
+                $query->orderBy('name', 'asc')->withCount('stores');
+            }])
+            ->withCount('stores')
+            ->get();
             
-            // Append the 'more' category at the end of the categories collection
-            if ($moreCategory) {
-                $categories->push($moreCategory);
-            }
             if ($categories->count() == 0) {
                 $data = [
                     'status' => 200,
@@ -66,6 +64,7 @@ class CategoryController extends Controller
                 'data' => [
                     'main_banner' => getBannerImageUrl($page),
                     'categories' => CategoryResource::collection($categories),
+                    'more_categories' => CategoryResource::collection($moreCategories),
                     'meta_data' => [
                         "next" => $categories->nextPageUrl(),
                         "previous" => $categories->previousPageUrl(),
