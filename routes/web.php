89b2ab34dbde886633d079c2a3fc84d88c07152f
Hamid Shaukat Amin <80332339+Akhwandzada1@users.noreply.github.com>
2023-11-16 12:09:41 +0500

    Duplicated code has been removed from importer controller as the code is already present in CJ Importer

--- routes/web.php
+++ routes/web.php
@@ -113,8 +113,6 @@ Route::namespace('App\Http\Controllers\Admin')
 
         // Importers
         Route::post('importer/import', [App\Http\Controllers\Admin\ImporterController::class, 'import'])->name('importer.import');
-        Route::get('importer/commissions', [App\Http\Controllers\Admin\ImporterController::class, 'import_commissions'])->name('importer.commissions');
-        Route::get('importer/vouchers', [App\Http\Controllers\Admin\ImporterController::class, 'import_coupons'])->name('importer.vouchers');
         Route::post('importer/save_settings', [App\Http\Controllers\Admin\ImporterController::class, 'saveSettings'])->name('importer.save_settings');
         Route::get('importer/importer_setting_form/{id}', [App\Http\Controllers\Admin\ImporterController::class, 'importerSettingForm'])->name('importer.importer_setting_form');
         Route::resource('importer', ImporterController::class)->only(['index', 'create', 'show']);
