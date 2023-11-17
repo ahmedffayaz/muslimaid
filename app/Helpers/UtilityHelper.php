c6cb0c0ba55b4e43baf7a0de4f08fca0162c18a8
Ma Mingxin <muhammad.umair@therightsw.com>
2023-11-17 12:28:19 +0500

    Resolve the issue ['meta_title'] veriable not set on login or register page

--- app/Helpers/UtilityHelper.php
+++ app/Helpers/UtilityHelper.php
@@ -801,7 +801,7 @@ function getSocialSeo($seoRule, $url){
         }
     }
 
-    if (!is_null($seoRule['meta_title'])) {
+    if (isset($seoRule['meta_title']) && !is_null($seoRule['meta_title'])) {
         $seoMetaTitle = $seoRule['meta_title'];
     }
 
@@ -811,29 +811,29 @@ function getSocialSeo($seoRule, $url){
             $pathInfo = $image != null ? pathinfo($image) : null;
             $extension = $pathInfo != null ? $pathInfo['extension'] : null;
             $description = $record->excerpt != null ? strip_tags($record->excerpt) : strip_tags($record->description);
-            $title = $record->title; 
+            $title = $record->title;
         } else if ($model == "\App\Models\Blog"){
             $image = getImageUrl($record->featured_image);
             $pathInfo = $image != null ? pathinfo($image) : null;
-            $extension = $pathInfo != null ? $pathInfo['extension'] : null; 
+            $extension = $pathInfo != null ? $pathInfo['extension'] : null;
             $description = $record->excerpt != null ? strip_tags($record->excerpt) : strip_tags($record->title);
             $title = $record->title;
         } else if ($model == "\App\Models\Appeal"){
-            $image = $record->image_type == "upload" ? getImageUrl($record->image_upload) : getImageUrl($record->image_link); 
+            $image = $record->image_type == "upload" ? getImageUrl($record->image_upload) : getImageUrl($record->image_link);
             $pathInfo = $image != null ? pathinfo($image) : null;
-            $extension = $pathInfo != null ? $pathInfo['extension'] : null; 
+            $extension = $pathInfo != null ? $pathInfo['extension'] : null;
             $description = $record->excerpt != null ? strip_tags($record->excerpt) : strip_tags($record->title);
             $title = $record->title;
         } else if ($model == "\App\Models\Category"){
             $image = $record->logo_type == "link" ? getImageUrl($record->logo_link) : getImageUrl($record->logo_upload);
             $pathInfo = $image != null ? pathinfo($image) : null;
-            $extension = $pathInfo != null ? $pathInfo['extension'] : null; 
+            $extension = $pathInfo != null ? $pathInfo['extension'] : null;
             $description = $record->description != null?  strip_tags($record->description) : strip_tags($record->name);
-            $title = $record->name; 
+            $title = $record->name;
         } else if ($model == "\App\Models\Store"){
             $image = getImageUrl($record->logo()->first()->image);
             $pathInfo = $image != null ? pathinfo($image) : null;
-            $extension = $pathInfo != null ? $pathInfo['extension'] : null; 
+            $extension = $pathInfo != null ? $pathInfo['extension'] : null;
             $description = $record->description != null ? strip_tags($record->description) : strip_tags($record->name);
             $title = $record->name;
         }
