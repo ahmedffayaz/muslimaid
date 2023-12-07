83768ee8ac5ba31de6386db534a3fb515502d88b
Ma Mingxin <muhammad.umair@therightsw.com>
2023-12-07 17:04:32 +0500

    Send email and push notification on donate cashback to appeal

--- app/Http/Controllers/Frontend/SearchController.php
+++ app/Http/Controllers/Frontend/SearchController.php
@@ -9,24 +9,23 @@ use App\Http\Controllers\Controller;
 
 class SearchController extends Controller
 {
-    public function index(Request $request, Store $stores)
+    public function index(Request $request)
     {
         $page = Page::where('slug', 'search')->whereIn('type', ['special', 'general'])->first();
         if (empty($page)) abort(404);
-        $stores = $stores->newQuery();
+        $stores = Store::select('id', 'name', 'slug', 'status', 'created_at')->where('status', 'active');
         $term = null;
         $search = $request->input('search');
 
         if ($search) {
             $stores->where('name', 'like', '%' . str_replace(' ', '%', $request->input('search')) . '%')
-            ->where('status', 'active')
             ->orWhereHas('storeRuleData', function ($query) use ($search) {
                 $query->where('key', 'meta:keywords')->where('value', 'like', '%' . $search . '%');
             });
             $term = $request->input('search');
         }
 
-        $stores = $stores->latest()->paginate(20);
+        $stores = $stores->withCount('cashbacks')->latest()->paginate(20);
 
         return view('frontend.pages.search', compact('stores', 'term', 'page'));
     }
@@ -35,7 +34,8 @@ class SearchController extends Controller
     {
         if (empty($request->input('search'))) return null;
 
-        $stores = Store::where('name', 'like', '%' . str_replace(' ', '%', $request->input('search')) . '%')
+        $stores = Store::select('id', 'name', 'slug', 'status', 'created_at')
+            ->where('name', 'like', '%' . str_replace(' ', '%', $request->input('search')) . '%')
             ->orWhereHas('storeRuleData', function ($query) use ($request) {
                 $query->where('key', 'meta:keywords')->where('value', 'like', '%' . $request->input('search') . '%');
             })->whereStatus('active')->limit(20)->get();
