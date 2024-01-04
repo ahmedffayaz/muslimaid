7a4bc1cad895ed6d115bf807692f2579b8e94513
M Umair <muhammad.umair@therightsw.com>
2024-01-04 18:00:05 +0500

    Display charity type column in charity list page in admin panel

--- resources/views/admin-dashboard/charities/index_data.blade.php
+++ resources/views/admin-dashboard/charities/index_data.blade.php
@@ -2,15 +2,13 @@
     <div class="nk-tb-list nk-tb-ulist">
         @if(count($charities))
         <div class="nk-tb-item nk-tb-head">
-            
-            
             <div class="nk-tb-col "><span class="sub-text">Title</span></div>
             <div class="nk-tb-col "><span class="sub-text">Description</span></div>
+            <div class="nk-tb-col "><span class="sub-text">Charity Type</span></div>
             <div class="nk-tb-col "><span class="sub-text">Country</span></div>
             <div class="nk-tb-col text-center"><span class="sub-text">Status</span></div>
             <div class="nk-tb-col nk-tb-col-tools text-right">
                 <span class="sub-text">Action</span>
-            
             </div>
         </div><!-- .nk-tb-item -->
         @foreach ($charities as $charity)
@@ -22,14 +20,16 @@
                 <span>{{ substr(strip_tags($charity->description),0,50) }}..</span>
             </div>
             <div class="nk-tb-col">
+                <span>{{ optional($charity->charity_type)->title }}</span>
+            </div>
+            <div class="nk-tb-col">
                 <span>{{ $charity->Country->name }}</span>
             </div>
             <div class="nk-tb-col text-center">
                 {!!$charity->status ==1  ? '<span class="tb-status badge badge-success">Active</span>' : '<span class="tb-status badge badge-warning">In-active</span>'!!}
-        
             </div>
             <div class="nk-tb-col nk-tb-col-tools">
-                <ul class="nk-tb-actions gx-1">  
+                <ul class="nk-tb-actions gx-1">
                     <li>
                         <div class="drodown">
                             <a href="#" class="dropdown-toggle btn btn-icon btn-trigger" data-toggle="dropdown"><em class="icon ni ni-more-h"></em></a>
@@ -37,30 +37,26 @@
                                 <ul class="link-list-opt no-bdr">
                                     <li><a href="{{route(getAdminPrefix() . '.charities.edit',$charity)}}"><em class="icon ni ni-edit"></em><span>Edit Charity</span></a></li>
                                     <li><a  class='delete'  form_id = "delete-{{$charity->id}}"   style="cursor: pointer"> <em class="icon ni ni-trash-fill"></em><span>Delete Charity</span></a>
-                                                            
                                         <form action="{{ route(getAdminPrefix() . '.charities.destroy',$charity) }}" id="delete-{{$charity->id}}" method="POST" class="m-0">
                                             @method('DELETE')
                                             @csrf
-                                            
                                         </form>
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                    </li> 
+                    </li>
                 </ul>
             </div>
         </div><!-- .nk-tb-item -->
-     @endforeach   
+     @endforeach
     </div><!-- .nk-tb-list -->
 </div><!-- .card-inner -->
 <div class="nk-block-between-md g-3 card-inner float-right">
     <div class="pagination g" >
-        {!! $charities->links()!!}                             
-                         
-    </div>    
-</div><!-- .nk-block-between -->     
-@else 
-    <h3 class="m-auto text-center py-5">No Charities found</h3> 
-@endif                             
-                    
+        {!! $charities->links()!!}
+    </div>
+</div><!-- .nk-block-between -->
+@else
+    <h3 class="m-auto text-center py-5">No Charities found</h3>
+@endif
